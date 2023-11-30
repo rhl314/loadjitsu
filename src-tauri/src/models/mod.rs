2bff4c18e93de3907851ff17aa1a70261632074d
Rahul T <rahulti314@gmail.com>
2023-11-30 17:25:18 +0530

    add struct for run

--- src-tauri/src/models/mod.rs
+++ src-tauri/src/models/mod.rs
@@ -1,9 +1,11 @@
 pub mod document_meta;
 pub mod document_revision;
+pub mod run;
 pub mod run_document_file;
 pub mod run_response_document;
 
 pub use document_meta::DocumentMeta;
 pub use document_revision::DocumentRevision;
+pub use run::Run;
 pub use run_document_file::RunDocumentFile;
 pub use run_response_document::RunResponseDocument;
---
+++ src-tauri/src/models/run.rs
@@ -0,0 +1,8 @@
+pub struct Run {
+    pub id: String,
+    pub document_revision_id: String,
+    pub pid: Option<String>,
+    pub status: String,
+    pub started_at: Option<String>,
+    pub completed_at: Option<String>,
+}
