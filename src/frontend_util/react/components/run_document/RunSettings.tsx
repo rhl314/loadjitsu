54cc60596dfd9d533c25f91faaca7c74253dbb1c
Rahul T <rahulti314@gmail.com>
2023-12-26 11:28:21 +0530

    setup skeleton for curl importer

--- src/frontend_util/react/components/run_document/RunSettings.tsx
+++ src/frontend_util/react/components/run_document/RunSettings.tsx
@@ -12,109 +12,23 @@ import { show } from "@tauri-apps/api/app";
 import { Fragment, useRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
-
-function TitleModal() {
-  const [open, setOpen] = useState(true);
-
-  const cancelButtonRef = useRef(null);
-
-  return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog
-        as="div"
-        className="relative z-10"
-        initialFocus={cancelButtonRef}
-        onClose={setOpen}
-      >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <ExclamationTriangleIcon
-                        className="h-6 w-6 text-red-600"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
-                        Deactivate account
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-gray-500">
-                          Are you sure you want to deactivate your account? All
-                          of your data will be permanently removed. This action
-                          cannot be undone.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                    onClick={() => setOpen(false)}
-                  >
-                    Deactivate
-                  </button>
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                    onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
-  );
-}
+import ImportCurl from "./ImportCurl";
 
 export default function RunSettings() {
   const navigate = useNavigate();
-  const [show, setShow] = useState(false);
+  const [showTitleModal, setShowTitleModal] = useState(false);
+  const [openCurlImporter, setOpenCurlImporter] = useState(false);
   const [error, setError] = useState<string>("");
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleClose = () => setShowTitleModal(false);
+  const handleShow = () => setShowTitleModal(true);
 
   const { state, dispatch } = useContext(RunDocumentAppContext);
   const configuration = state.runDocument?.configuration;
 
   function TitleModal() {
     return (
-      <Transition.Root show={show} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={setShow}>
+      <Transition.Root show={showTitleModal} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setShowTitleModal}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -184,7 +98,7 @@ export default function RunSettings() {
                     <button
                       type="button"
                       className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                      onClick={() => setShow(false)}
+                      onClick={() => setShowTitleModal(false)}
                     >
                       Back
                     </button>
@@ -211,10 +125,10 @@ export default function RunSettings() {
   };
   const startExecution = async () => {
     if (_.isEmpty(_.trim(state?.runDocument?.title))) {
-      setShow(true);
+      setShowTitleModal(true);
       return;
     }
-    setShow(false);
+    setShowTitleModal(false);
     console.log(state.runDocument);
     if (_.isNil(state.runDocument)) {
       return;
@@ -255,6 +169,14 @@ export default function RunSettings() {
         <div className="grid">
           <div className="tabs z-10 -mb-px">
             <button className="tab tab-lifted tab-active">Settings</button>
+            <button
+              className="tab tab-lifted btn-sm btn-primary"
+              onClick={() => {
+                setOpenCurlImporter(true);
+              }}
+            >
+              Import curl
+            </button>
           </div>
           <div className="bg-base-300  relative overflow-x-auto">
             <div className="preview border-base-300 bg-base-100  min-h-[6rem] w-100 gap-2 overflow-x-hidden border bg-cover bg-top p-4">
@@ -336,6 +258,7 @@ export default function RunSettings() {
         </div>
       </div>
       <TitleModal />
+      <ImportCurl open={openCurlImporter} setOpen={setOpenCurlImporter} />
     </>
   );
 }
