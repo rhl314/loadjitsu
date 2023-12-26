664bf874422db0e240d39a53de3d0cc36f37edbc
Rahul T <rahulti314@gmail.com>
2023-12-26 09:55:33 +0530

    force title
    n

--- src/frontend_util/react/components/run_document/RunSettings.tsx
+++ src/frontend_util/react/components/run_document/RunSettings.tsx
@@ -7,6 +7,99 @@ import PlaySvg from "../../../../assets/svg/play.svg?react";
 import { RunDocument } from "../../../ipc/run_document";
 import { RunDocumentAppContext } from "../../RunDocumentContext";
 import { useNavigate } from "react-router-dom";
+import { show } from "@tauri-apps/api/app";
+
+import { Fragment, useRef } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+
+function TitleModal() {
+  const [open, setOpen] = useState(true);
+
+  const cancelButtonRef = useRef(null);
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationTriangleIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        Deactivate account
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Are you sure you want to deactivate your account? All
+                          of your data will be permanently removed. This action
+                          cannot be undone.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                  >
+                    Deactivate
+                  </button>
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
 
 export default function RunSettings() {
   const navigate = useNavigate();
@@ -17,6 +110,93 @@ export default function RunSettings() {
 
   const { state, dispatch } = useContext(RunDocumentAppContext);
   const configuration = state.runDocument?.configuration;
+
+  function TitleModal() {
+    return (
+      <Transition.Root show={show} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setShow}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <ExclamationTriangleIcon
+                          className="h-6 w-6 text-green-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left w-full">
+                        <Dialog.Title
+                          as="h3"
+                          className="text-base font-semibold leading-6 text-gray-900"
+                        >
+                          We need a name for this test
+                        </Dialog.Title>
+                        <div className="mt-2 w-full">
+                          <input
+                            type="text"
+                            value={state.runDocument.title}
+                            onChange={(event: any) => {
+                              (state.runDocument.title = event?.target?.value),
+                                dispatch({
+                                  runDocument: {
+                                    ...state.runDocument,
+                                  },
+                                });
+                            }}
+                            placeholder="Type here"
+                            className="input input-bordered w-full"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                      onClick={() => startExecution()}
+                    >
+                      Save and run
+                    </button>
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => setShow(false)}
+                    >
+                      Back
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    );
+  }
   const displayError = () => {
     if (_.isEmpty(_.trim(error))) {
       return null;
@@ -30,6 +210,11 @@ export default function RunSettings() {
     );
   };
   const startExecution = async () => {
+    if (_.isEmpty(_.trim(state?.runDocument?.title))) {
+      setShow(true);
+      return;
+    }
+    setShow(false);
     console.log(state.runDocument);
     if (_.isNil(state.runDocument)) {
       return;
@@ -150,6 +335,7 @@ export default function RunSettings() {
           </div>
         </div>
       </div>
+      <TitleModal />
     </>
   );
 }
@@ -174,23 +360,17 @@ const Old = () => {
               <div className="col-4">
                 <div className="text-center mt-4">Test duration in seconds</div>
               </div>
-              <div
-                className="col-3"
-                onClick={() => {
-                  /*setShow(true);*/
-                }}
-              >
+              <div className="col-3">
                 <div className="mx-auto" style={{ width: "4rem" }}>
                   <PlaySvg />
                 </div>
                 <div className="text-center mt-4">Start Test</div>
               </div>
             </div>
-            {/*displayError()*/}
           </div>
         </div>
       </div>
-      <Modal show={false} onHide={() => {}} centered size="lg">
+      <Modal show={true} onHide={() => {}} centered size="lg">
         <Modal.Header closeButton>
           <Modal.Title>Please confirm</Modal.Title>
         </Modal.Header>
