4aed26c38269d54f8c8d51241fe29d73b56e989a
Rahul T <rahulti314@gmail.com>
2024-06-13 14:13:58 +0530

    add landing page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,8 +1,10 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import RetroGrid from "../components/magicui/RetroGrid";
 import TopNav from "../frontend_util/react/components/TopNav";
 import { IRunDocumentFile } from "../types/common";
+import ShimmerButton from "../components/magicui/ShimmerButton";
 
 const Welcome = () => {
   const navigate = useNavigate();
@@ -21,12 +23,26 @@ const Welcome = () => {
     navigate(`/runs/api/${temporaryDocumentPath}`);
   };
   return (
-    <>
-      <div>
-        <TopNav />
-        <h1>Hellow world</h1>
+    <div
+      className="relative flex flex-col h-full w-full  items-center justify-center overflow-hidden rounded-lg  bg-background p-20 md:shadow-xl"
+      style={{ height: "100vh" }}
+    >
+      <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-6xl font-bold leading-none tracking-tighter text-transparent">
+        Welcome !, <br />
+        let the adventure begin!
+      </span>
+
+      <div className="z-10 flex min-h-[16rem] items-center justify-center">
+        <ShimmerButton className="shadow-2xl">
+          <span className="whitespace-pre-wrap text-center text-xl font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-2xl">
+            Start your first load test
+          </span>
+        </ShimmerButton>
       </div>
-    </>
+      <p className="text-white text-xl">or Import from curl</p>
+
+      <RetroGrid />
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        grid: "grid 50s linear infinite",
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+        slide: "slide var(--speed) ease-in-out infinite alternate",
+      },
+      keyframes: {
+        grid: {
+          "0%": { transform: "translateY(-50%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        slide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
