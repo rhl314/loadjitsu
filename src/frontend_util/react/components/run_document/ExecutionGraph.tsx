2b8d51ee930d1196680ed1da142e8e95de6bc209
Rahul T <rahulti314@gmail.com>
2024-06-17 15:48:22 +0530

    link run again

--- src/frontend_util/react/components/run_document/ExecutionGraph.tsx
+++ src/frontend_util/react/components/run_document/ExecutionGraph.tsx
@@ -156,43 +156,49 @@ const ExecutionGraph = () => {
               <div className="bg-base-300  relative overflow-x-auto">
                 <div className="preview border-base-300 bg-base-100  min-h-[6rem] w-100 gap-2 overflow-x-hidden border bg-cover bg-top p-4">
                   {state.executionDocument?.status === "RUNNING" && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="red"
-                      className="w-[60px] mx-auto mt-3"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.5 7.5a3 3 0 013-3h9a3 3 0 013 3v9a3 3 0 01-3 3h-9a3 3 0 01-3-3v-9z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                    <div>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="red"
+                        className="w-[60px] mx-auto mt-3"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.5 7.5a3 3 0 013-3h9a3 3 0 013 3v9a3 3 0 01-3 3h-9a3 3 0 01-3-3v-9z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      <div className="text-center">
+                        <p>Abort test</p>
+                      </div>
+                    </div>
                   )}
                   {(state.executionDocument?.status === "COMPLETED" ||
                     state.executionDocument?.status === "ABORTED") && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="green"
-                      className="w-[60px] mx-auto mt-3"
+                    <div
+                      className="cursor-pointer"
+                      onClick={() => {
+                        navigation(`/runs/api/${state.runDocumentPath || ""}`);
+                      }}
                     >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="green"
+                        className="w-[60px] mx-auto mt-3"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      <div className="text-center">
+                        <p>Run again</p>
+                      </div>
+                    </div>
                   )}
-                  <div className="text-center">
-                    {state.executionDocument?.status === "RUNNING" && (
-                      <p>Abort test</p>
-                    )}
-                    {(state.executionDocument?.status === "COMPLETED" ||
-                      state.executionDocument?.status === "ABORTED") && (
-                      <p>Run again</p>
-                    )}
-                  </div>
                 </div>
               </div>
             </div>
